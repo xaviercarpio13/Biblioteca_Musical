3257b835ee8ecb42e7ffc96e99573eceb4ac9db8
kelly-sangoluisa <Usuario@LAPTOP-CAV0ND72>
2024-05-16 02:58:59 -0500

    agregacion de primera funcion

--- src/Clases/Biblioteca.java
+++ src/Clases/Biblioteca.java
@@ -1,6 +1,11 @@
 package Clases;
 
+import Conexion.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import javax.swing.JOptionPane;
 
 public class Biblioteca {
     private List<Album> albumes;
@@ -20,8 +25,26 @@ public class Biblioteca {
         //mostar las canciones que tiene el album con su duracion
     }
     
-    public void agregarAlbum(Album album){
-        albumes.add(album);
+    public void agregarAlbum(Album alb){
+        try {
+            Connection cn = Conexion.getConection();
+            String query = "INSERT INTO album (nombreAlbum, anioLanzamientoAlbum, disqueraAlbum) VALUES (?,?,?)";
+
+            PreparedStatement insertar = cn.prepareStatement(query);
+
+            insertar.setString(1, alb.getNombreAlbum());
+            insertar.setInt(2, alb.getAnioLanzamiento());
+            insertar.setString(3, alb.getDisquera());
+
+            insertar.executeUpdate();
+            insertar.close();
+
+            JOptionPane.showInternalMessageDialog(null, "Registro guardado");
+
+        } catch (SQLException e) {
+            System.out.println("Error  al agregar al Album" + e.getMessage());
+        }
+       // albumes.add(dbo);
     }
     
     public Album buscarAlbumsPorAnio(int anio){
