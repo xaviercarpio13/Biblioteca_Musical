ce27e1dd4d2803187bafcaa8195678b6dd1f3e35
Xavier <xavier.carpio@epn.edu.ec>
2024-05-16 07:18:45 -0500

    Agregación de funcionalidad para agregar lista de aritstas en pantalla AgregarAlbum.

--- src/Conexion/BibliotecaMusical.sql
+++ src/Conexion/BibliotecaMusical.sql
@@ -1,11 +1,3 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  Kelly
- * Created: 16 may. 2024
- */
 
 create database BibliotecaMusical;
 use BibliotecaMusical;
