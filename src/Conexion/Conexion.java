5c521d83909edb8f644bc9e1310cff566f568f5e
kelly-sangoluisa <Usuario@LAPTOP-CAV0ND72>
2024-05-16 09:53:07 -0500

    arreglo de conexion

--- src/Conexion/Conexion.java
+++ src/Conexion/Conexion.java
@@ -10,7 +10,8 @@ import java.sql.DriverManager;
 public class Conexion {
     public static Connection getConection(){
             String URL = "jdbc:sqlserver://localhost:1433;"+"database=BibliotecaMusical;"
-            +"user=sa;"+"password=P@ssw0rd;"+ "encrypt=false;" + "trustServerCertificate=false;";
+            +"user=sa;"+"password=P@ssw0rd;"+ "encrypt=false;" + "trustServerCertificate=false;"+
+            "driverFile=./mysql-connector-java-8.0.29.jar";
             Connection con = null;
             try{
                 con = (Connection)DriverManager.getConnection(URL);
